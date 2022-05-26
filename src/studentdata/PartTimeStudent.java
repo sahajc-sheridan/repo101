e9bdc4a059fe04b72832070ea94fbd9553d2b456
sahajc-sheridan <chawsaha@sheridancollege.ca>
2022-05-26 10:08:35 -0400

    part time student file added

---
+++ src/studentdata/PartTimeStudent.java
@@ -0,0 +1,24 @@
+
+package studentdata;
+
+/**
+ *
+ * @author Sahaj Chawla
+ */
+public class PartTimeStudent {
+    private int numOfCourses; 
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+}
